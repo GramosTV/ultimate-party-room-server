8866a93cf624c3476a0eff179b3e7ab7fdc5ab89
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-10 22:21:17 +0200

    websocket messages init

---
+++ src/messages/messages.gateway.ts
@@ -0,0 +1,59 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class MessagesGateway {
+  @WebSocketServer()
+  server: Server;
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @SubscribeMessage('createMessage')
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    const message = await this.messagesService.create(createMessageDto);
+
+    this.server.emit('message', message);
+
+    return message;
+  }
+
+  @SubscribeMessage('findAllMessages')
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @SubscribeMessage('join')
+  joinRoom() {
+    return null; //TODO
+  }
+
+  @SubscribeMessage('typing')
+  async typing() {
+    return null; //TODO
+  }
+  // @SubscribeMessage('findOneMessage')
+  // findOne(@MessageBody() id: number) {
+  //   return this.messagesService.findOne(id);
+  // }
+
+  // @SubscribeMessage('updateMessage')
+  // update(@MessageBody() updateMessageDto: UpdateMessageDto) {
+  //   return this.messagesService.update(updateMessageDto.id, updateMessageDto);
+  // }
+
+  // @SubscribeMessage('removeMessage')
+  // remove(@MessageBody() id: number) {
+  //   return this.messagesService.remove(id);
+  // }
+}
---
+++ src/messages/messages.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  messages: Message[] = [{ name: 'XYZ', text: 'sup' }];
+
+  create(createMessageDto: CreateMessageDto) {
+    const message = { ...createMessageDto };
+    this.messages.push(createMessageDto);
+    return message;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} message`;
+  }
+
+  update(id: number, updateMessageDto: UpdateMessageDto) {
+    return `This action updates a #${id} message`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} message`;
+  }
+}

c68235c792918502865dc06794d0a2c05fd2e4ad
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-14 18:18:40 +0200

    db file restructure

--- src/db/modules/message.module.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
-import { Message } from '../entities/Message.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Message])],
-  exports: [TypeOrmModule],
-})
-export class MessageModule {}

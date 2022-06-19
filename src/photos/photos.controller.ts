4969376a870276b40b728851d0d155c4e06d5aaa
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-19 22:31:43 +0200

    Photos api all set up

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -1,21 +1,45 @@
 import {
   Controller,
+  Get,
+  Param,
   Post,
+  Req,
+  Response,
+  StreamableFile,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { UserService } from 'src/db/user/user.service';
 const byte = 1048576;
 @Controller('photos')
 export class PhotosController {
-  @Post()
+  constructor(private readonly userService: UserService) {}
+  @Get(':photo')
+  getFile(
+    @Param('photo') photo: string,
+    @Response({ passthrough: true }) res,
+  ): StreamableFile {
+    const file = createReadStream(
+      join(process.cwd(), `../upr-server/src/uploads/profilePictures/${photo}`),
+    );
+    res.set({
+      'Content-Type': 'image',
+    });
+    return new StreamableFile(file);
+  }
+
+  @Post(':clientId')
   @UseInterceptors(
     FileInterceptor('photo', {
-      dest: '../uploads/profilePictures',
+      dest: '../upr-server/src/uploads/profilePictures',
       limits: { fileSize: byte * 10 },
     }),
   )
-  uploadSingle(@UploadedFile() file) {
-    console.log('file saved successfully');
+  uploadSingle(@Param('clientId') clientId: string, @UploadedFile() file) {
+    this.userService.updateProfilePicture(clientId, file.filename);
   }
 }
--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/db/user/user.module';
+import { UserService } from 'src/db/user/user.service';
 import { PhotosController } from './photos.controller';
 
 @Module({
+  imports: [UserModule],
   controllers: [PhotosController],
+  providers: [UserService],
 })
 export class PhotosModule {}
